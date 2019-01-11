a169acbcf0caa466b3c36e9e8c188c43d98d62c1
Soulzerz <soulzerz@gmail.com>
2019-01-11 02:35:34 -0400

    Created blog basic schema

---
+++ mindspot/blog/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+# Create your models here.
+
+class Tag(models.Model):
+    """ Represents the Tag model """
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
+
+class Category(models.Model):
+    """ Represents the Category model """
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
+
+class Post(models.Model):
+    """ Represents the Post model """
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
